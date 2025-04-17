7ef9ce5e1a3f535504483f15fa1331824db5125c
Asifsaifali <asif263848@gmail.com>
2025-04-17 20:01:58 +0530

    made some changes

--- basic.py
+++ basic.py
@@ -10,4 +10,18 @@ def fun(a,b):
     # a,b = b-a,a-b
     print(a-b)
 
-fun(a,b)
+# fun(a,b)
+
+x =  ""
+s =  "y"
+
+y = ""
+if x == "":
+    print(s)
+for i in x:
+    for j in s:
+        if i==j:
+            continue
+        else:
+            y = j
+print(y)
